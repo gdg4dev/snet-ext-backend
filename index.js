9c4dd026eae8ec19bf7b0c3ad7b67de3dc887674
Aniket Hirpara <gdg4dev@gmail.com>
2024-09-14 14:54:48 -0400

    fixed urls.json not found

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { BloomFilter } = require('bloom-filters');
 const OpenAI = require('openai')
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -23,7 +24,7 @@ function normalizeURL(inputURL) {
   }
 }
 
-const urls = JSON.parse(fs.readFileSync('urls.json', 'utf-8'));
+const urls = JSON.parse(fs.readFileSync(path.join(__dirname, 'urls.json'), 'utf-8'));
 urls.forEach(inputURL => {
   const normalizedURL = normalizeURL(inputURL);
   bloomFilter.add(normalizedURL);
